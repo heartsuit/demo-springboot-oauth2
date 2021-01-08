8eea32d090461cd91f1014920a0ace83181e2e88
heartsuit <nxq0108@126.com>
2021-01-08 16:19:48 +0800

    authorization server: password

--- in-memory/springboot-oauth2-authorization-server/src/main/java/com/heartsuit/config/AuthorizationConfig.java
+++ in-memory/springboot-oauth2-authorization-server/src/main/java/com/heartsuit/config/AuthorizationConfig.java
@@ -1,11 +1,14 @@
 package com.heartsuit.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 
 /**
@@ -25,15 +28,21 @@ public class AuthorizationConfig extends AuthorizationServerConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
-    // http://localhost:9000/oauth/authorize?grant_type=implicit&response_type=token&scope=pc&client_id=client0&client_secret=secret0
+    /**
+     * password 密码模式需要在认证服务器中设置 中配置AuthenticationManager
+     * 否则报错：Unsupported grant type: password
+     */
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    // http://localhost:9000/oauth/token?username=admin&password=123456&grant_type=password&scope=read%20write&client_id=client1&client_secret=secret1
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         clients.inMemory()
-                .withClient("client0")
-                .secret(passwordEncoder().encode("secret0"))
-                .authorizedGrantTypes("implicit", "refresh_token") //Implicit grant type not supported from token endpoint
-                .scopes("read", "write")
-                .redirectUris("http://localhost:8000/public/hello");
+                .withClient("client1")
+                .secret(passwordEncoder().encode("secret1"))
+                .authorizedGrantTypes("password", "refresh_token")
+                .scopes("read", "write");
     }
 
     /**
@@ -44,5 +53,10 @@ public class AuthorizationConfig extends AuthorizationServerConfigurerAdapter {
         oauthServer.tokenKeyAccess("permitAll()")
                 .checkTokenAccess("isAuthenticated()")
                 .allowFormAuthenticationForClients();
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
+        endpoints.authenticationManager(authenticationManager);
     }
 }
--- in-memory/springboot-oauth2-authorization-server/src/main/java/com/heartsuit/config/SecurityConfig.java
+++ in-memory/springboot-oauth2-authorization-server/src/main/java/com/heartsuit/config/SecurityConfig.java
@@ -1,7 +1,9 @@
 package com.heartsuit.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -23,5 +25,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .withUser("admin").password(passwordEncoder.encode("123456")).roles("ADMIN")
                 .and()
                 .withUser("user").password(passwordEncoder.encode("123456")).roles("USER");
+    }
+
+    /**
+     * password 密码模式需要在认证服务器中设置 中配置AuthenticationManager
+     * 否则报错：Unsupported grant type: password
+     */
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
