76f06ff96fa10064d022d10c88b9b9c39117ea09
heartsuit <nxq0108@126.com>
2021-01-08 15:44:18 +0800

    resource server

---
+++ in-memory/springboot-oauth2-resource-server/src/main/java/com/heartsuit/SpringbootOauth2ResourceServerApplication.java
@@ -0,0 +1,13 @@
+package com.heartsuit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootOauth2ResourceServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootOauth2ResourceServerApplication.class, args);
+    }
+
+}
---
+++ in-memory/springboot-oauth2-resource-server/src/main/java/com/heartsuit/config/ResourceServerConfig.java
@@ -0,0 +1,26 @@
+package com.heartsuit.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+/**
+ * @Author Heartsuit
+ * @Date 2021-01-08
+ */
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        // Since we want the protected resources to be accessible in the UI as well we need session creation to be allowed (it's disabled by default in 2.0.6)
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+                .and().requestMatchers()
+                .anyRequest().and().anonymous().and().authorizeRequests()
+                .antMatchers("/private/read/**").access("#oauth2.hasScope('read') and hasRole('ROLE_USER')")
+                .antMatchers("/private/write/**").access("#oauth2.hasScope('write') and hasRole('ROLE_ADMIN')")
+                .antMatchers("/private/**").authenticated();
+    }
+}
